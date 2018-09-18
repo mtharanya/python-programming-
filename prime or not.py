76eed5d294bb259999007a21eabf0d220835575f
mtharanya <42570549+mtharanya@users.noreply.github.com>
2018-09-18 17:19:50 +0530

    Create prime or not.py

---
+++ prime or not.py
@@ -0,0 +1,11 @@
+num = 89
+num = int(input("Enter a number: "))
+if num > 1:
+if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+else:
+   print(num,"is not a prime number")
