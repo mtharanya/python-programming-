9b1a46ae35684d9a5a9038abf9f4ce2e83ef6448
mtharanya <42570549+mtharanya@users.noreply.github.com>
2018-09-19 17:21:12 +0530

    Update minimum.py

--- minimum.py
+++ minimum.py
@@ -1,1 +1,23 @@
+while True:
+    inp = raw_input('Enter a number: ')
+    if inp == 'done' : 
+        break
+
+    try:
+        num = float(inp)
+    except:
+        print 'Invalid input'
+        continue                            
+
+numbers = list(num)
+minimum = None       
+
+
+for num in numbers :                          
+    if minimum == None or num < minimum :
+        minimum = num
+
+   print 'Minimum:', minimum
+
+
 
