11284dfa9d23564e0b8854e58679674646351d04
mtharanya <42570549+mtharanya@users.noreply.github.com>
2018-09-27 16:30:32 +0530

    Create special character.py

---
+++ special character.py
@@ -0,0 +1,12 @@
+def char freqency(str1):
+  dict={}
+ for n is str 1:
+  keys=dict.keys()
+if n in keys:
+  dict[n]+=1
+else:
+   dict[n]=1
+ return dist
+    print(char frequency('gmail.com'))
+    
+ 
