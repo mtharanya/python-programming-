304c162dae38e5f23ed4bb821b694418cbd21e7e
mtharanya <42570549+mtharanya@users.noreply.github.com>
2018-09-27 16:48:17 +0530

    Create swap2number.py

---
+++ swap2number.py
@@ -0,0 +1,10 @@
+# x=input('enter value of x:')
+# y=input('enter value of y:')
+   x=4
+   y=10
+#create a tempory variable and swap the value
+  temp=x
+  x=y
+  y=temp
+ print('the value of x after swapping:{}'.formate)
+ print('the value of y after swapping:{}'.formate)
