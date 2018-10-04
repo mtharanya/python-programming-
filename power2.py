ac0ed948da9164c03294eae3ceb8f354a6692100
mtharanya <42570549+mtharanya@users.noreply.github.com>
2018-10-04 17:40:50 +0530

    Create power2.py

---
+++ power2.py
@@ -0,0 +1,8 @@
+n=int(input("enter the number:"))
+if (n == 0): 
+      print("False")
+elif(n % 2)==0:
+        print("the number is power two")
+else:            
+    print("the number is not power two")
+  
